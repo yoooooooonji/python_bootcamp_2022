cefad78391a57709d3d343b880d52035f9e60afc
Yoonji <72339970+yoooooooonji@users.noreply.github.com>
2022-09-07 08:12:26 +0900

    Update Quiz.py

--- day-17/Quiz.py
+++ day-17/Quiz.py
@@ -1,5 +1,6 @@
-from question_model_day_17 import Question
-from data_day_17 import question_data
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -8,4 +9,4 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank[0])
+while quiz.
