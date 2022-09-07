514f12f08a7b3c7f1e6bfba1aa48644f12894156
Yoonji <72339970+yoooooooonji@users.noreply.github.com>
2022-09-07 23:17:47 +0900

    Update Quiz.py

--- day-17/Quiz.py
+++ day-17/Quiz.py
@@ -9,4 +9,10 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-while quiz.
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{question_bank}")
